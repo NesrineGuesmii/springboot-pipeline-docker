9faea667057a82e957c65460ab977c1a252cdb97
nesrine_guesmi <70206287+NesrineGuesmii@users.noreply.github.com>
2023-12-04 09:24:23 +0100

    updates

--- src/main/java/com/alibou/springdemo/student/DBStudentService.java
+++ src/main/java/com/alibou/springdemo/student/DBStudentService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class DBStudentService implements StudentService {
 
-  //modif 
+  //modif pour test
   private final StudentRepository repository;
 
   public DBStudentService(StudentRepository repository) {
